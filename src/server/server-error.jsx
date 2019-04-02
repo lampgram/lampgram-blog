51bfb7d700bd2575852f81cbff9dfe77b98400d4
Kate <k.karpo.a@gmail.com>
2019-04-02 08:58:41 +0300

    Create server-error.jsx

---
+++ src/server/server-error.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class ServerError extends Component {
+
+  render() {
+    return (
+      <div className="float-center" style={{width: '640px', textAlign: 'center'}}>
+        <img width="640px" height="480px" src="/images/501.jpg"/>
+        <div>
+          <h4>Sorry.</h4>
+          <p>Looks like something went wrong on our end.</p>
+          <p>Head back to <a href="/">Smoke</a> homepage.</p>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default ServerError;
